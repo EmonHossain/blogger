f8674184306b440fa75ac02c5e40ebc945e52e5a
Emon Hossain <emondipu@gmail.com>
2017-08-24 11:56:54 +0600

    audit is modified

--- src/main/java/com/shareknowledge/audit/AuditorAwareImpl.java
+++ src/main/java/com/shareknowledge/audit/AuditorAwareImpl.java
@@ -1,6 +1,5 @@
 package com.shareknowledge.audit;
 
-import org.springframework.boot.autoconfigure.security.AuthenticationManagerConfiguration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +7,7 @@ import org.springframework.stereotype.Component;
  * Created by Emon Hossain on 8/18/2017.
  */
 @Component
-public class AuditorAwareImpl implements AuditorAware{
+public class AuditorAwareImpl implements AuditorAware<Integer>{
 
     @Override
     public Integer getCurrentAuditor() {
