967d1d37b211950eccb5f715f9cdf8a6a5e41454
Emon Hossain <emondipu@gmail.com>
2017-08-24 14:39:33 +0600

    generic serevice is added

--- src/main/java/com/shareknowledge/image/ImageService.java
+++ src/main/java/com/shareknowledge/image/ImageService.java
@@ -1,7 +1,9 @@
 package com.shareknowledge.image;
 
+import com.shareknowledge.util.BaseService;
+
 /**
  * Created by Emon Hossain on 8/23/2017.
  */
-public interface ImageService {
+public interface ImageService extends BaseService {
 }
