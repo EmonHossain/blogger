08d64c72b1d1b4b8610c62b91fc3b1ec309e638a
Emon Hossain <emondipu@gmail.com>
2017-10-24 14:20:38 +0600

    audit creation in complete

--- src/main/java/com/shareknowledge/audit/AuditorAwareImpl.java
+++ src/main/java/com/shareknowledge/audit/AuditorAwareImpl.java
@@ -14,6 +14,8 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 	@Override
 	public String getCurrentAuditor() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth == null || !auth.isAuthenticated())
+			return null;
 		return auth.getName();
 	}
 }
---
+++ src/main/java/com/shareknowledge/auth/CustomAuthenticationProvider.java
@@ -0,0 +1,22 @@
+package com.shareknowledge.auth;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider{
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = authentication.getName();
+		String password = authentication.getCredentials().toString();
+		return new UsernamePasswordAuthenticationToken(null, null, null);
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+	
+}
